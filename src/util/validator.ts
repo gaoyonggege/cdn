937974a5ac0ecdeaf03ef619c714ba1806eb1ccb
gaoyong <gaoyong@weiyankeji.cn>
2019-03-21 19:29:32 +0800

    新增校验 包括拓展第三方包声明

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -5,14 +5,32 @@
 */
 import { ProjectConfig } from '../model/projectConfig';
 import { runInNewContext } from 'vm';
-
+import { clouds } from '../type';
 
 export function validateConfig ( projectCofig: ProjectConfig ) {
     if ( !projectCofig ) {
         throw new Error('projectCofig empty');
     }
 
+    if ( !clouds.includes(projectCofig.type) ) {
+        throw new Error(`cdnConfig.json file type error`);
+    }
 
+    if ( !projectCofig.root || /[^a-zA-Z0-9]/.test(projectCofig.root) ) {
+        throw new Error(`cdnConfig.json file root error`);
+    }
+
+    if ( !projectCofig.name ) {
+        throw new Error(`cdnConfig.json file name error`);
+    }
+
+    if ( !projectCofig.version ) {
+        throw new Error(`cdnConfig.json file version error`);
+    }
+
+    if ( !projectCofig.staticDir || projectCofig.staticDir.startsWith('..') ) {
+        throw new Error(`cdnConfig.json file staticDir error`);            
+    }
 
     return true;
 }
